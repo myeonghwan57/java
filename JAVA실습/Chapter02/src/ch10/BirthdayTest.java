454827edb188448fa245cfdccf000873a43bcc57
myeonghwna57 <myeonghwan57@gmail.com>
2023-07-13 17:33:51 +0900

    접근제어지시자 정보은닉

---
+++ JAVA실습/Chapter02/src/ch10/BirthdayTest.java
@@ -0,0 +1,16 @@
+package ch10;
+
+public class BirthdayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Birthday date = new Birthday();
+		date.setYear(2023);
+		date.setMonth(13);
+		date.setDay(30);
+		
+		date.showDate();
+	}
+
+}
