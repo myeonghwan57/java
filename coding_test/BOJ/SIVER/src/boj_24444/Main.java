1790982bd124f19299f2200f510e219d460f07f8
myeonghwna57 <myeonghwan57@gmail.com>
2023-08-12 16:45:35 +0900

    파일 생성

---
+++ coding_test/BOJ/SIVER/src/boj_24444/Main.java
@@ -0,0 +1,19 @@
+package boj_24444;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int r = sc.nextInt();
+		
+		
+		
+	}
+
+}
