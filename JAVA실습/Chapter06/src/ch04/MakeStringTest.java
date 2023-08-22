a8085c84459f7d35bce38a979eb6c034a1e9e070
myeonghwna57 <myeonghwan57@gmail.com>
2023-08-22 14:31:16 +0900

    java 람다식 구현방법 비교

---
+++ JAVA실습/Chapter06/src/ch04/MakeStringTest.java
@@ -0,0 +1,12 @@
+package ch04;
+
+public class MakeStringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringConcat S = (s1, s2) -> System.out.println(s1+s2);
+		S.makeString("안녕", "하시렵니까?");
+	}
+
+}
