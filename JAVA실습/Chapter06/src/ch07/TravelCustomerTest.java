263a2978d96ea89f7f56d3db77888a59d1b5b9ab
myeonghwna57 <myeonghwan57@gmail.com>
2023-08-31 22:21:41 +0900

    java 스트림 활용

---
+++ JAVA실습/Chapter06/src/ch07/TravelCustomerTest.java
@@ -0,0 +1,30 @@
+package ch07;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TravelCustomerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TravelCustomer customerlee = new TravelCustomer("LEE", 30, 100);
+		TravelCustomer customerkim = new TravelCustomer("KIM", 29, 190);
+		TravelCustomer customerpark = new TravelCustomer("PARK", 19, 50);
+		
+		ArrayList<TravelCustomer> customerList = new ArrayList<>();
+		customerList.add(customerpark);
+		customerList.add(customerkim);
+		customerList.add(customerlee);
+		
+		System.out.println("고객 명단 출력");
+		customerList.stream().map(s->s.getName()).forEach(s->System.out.println(s));
+	
+		System.out.println("여행 비용");
+		System.out.println(customerList.stream().mapToInt(c->c.getPrice()).sum());
+	
+		System.out.println("나이가 20세 이상인 사람");
+		customerList.stream().filter(c->c.getAge() >= 20).map(c->c.getName()).sorted().forEach(s->System.out.println(s));
+	}
+
+}
