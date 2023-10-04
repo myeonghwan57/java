efb60aad157815218c8046de5298dc2274173190
myeonghwna57 <myeonghwan57@gmail.com>
2023-10-04 19:18:31 +0900

    java stream 학습

--- JAVA실습/Chapter06/src/ch05/ArrayListStreamTest.java
+++ JAVA실습/Chapter06/src/ch05/ArrayListStreamTest.java
@@ -1,6 +1,7 @@
 package ch05;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -17,12 +18,19 @@ public class ArrayListStreamTest {
 		Stream<String> stream = sList.stream();
 		stream.forEach(s->System.out.println(s));
 		System.out.println();
+		
 		sList.stream().sorted().forEach(s->System.out.println(s));
 		System.out.println();
+		
+		sList.stream().sorted(Comparator.reverseOrder()).forEach(s->System.out.println(s)); //역순
+		System.out.println();
+		
 		sList.stream().map(s->s.length()).forEach(s->System.out.println(s));
 		System.out.println();
+		
 		sList.stream().filter(s->s.length() >= 5).forEach(n->System.out.println(n));
 		System.out.println();
+		
 		Stream.of(sList).forEach(s-> System.out.println(s));
 		
 	}
--- JAVA실습/Chapter06/src/ch05/IntArrayStreamTest.java
+++ JAVA실습/Chapter06/src/ch05/IntArrayStreamTest.java
@@ -8,19 +8,25 @@ public class IntArrayStreamTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] arr = {1,2,5,4,3};
-
-		Arrays.stream(arr).sorted().forEach(System.out::println);
+		int[] arr = {1,2,5,4,3}; // 배열 생성 
 		
+		for (int num: arr) {
+			System.out.println(num);
+		}
+		System.out.println();
+		
+		Arrays.stream(arr).sorted().forEach(System.out::println);
+						//중간연산		//최종연산 으로 구분 됨 -> 최종연산은 마지막에 하나만 적용됨. -> 중간연산에 대한 결과를 알수 없음. 
 		System.out.println();
 		IntStream is = Arrays.stream(arr);
-		is.forEach(n-> System.out.println(n));
+		is.forEach(n-> System.out.println(n)); //한번 사용한 is는 재사용 불가 새로운 스트림 생성해야함.
 		
 		int sum = Arrays.stream(arr).sum();
+		long cnt = Arrays.stream(arr).count();
 		System.out.println(sum);
+		System.out.println(cnt);
 		
-	
-		
+
 	}
 
 }
