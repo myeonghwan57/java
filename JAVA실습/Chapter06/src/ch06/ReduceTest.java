70c8c16ee70cbedf574c08bf891612322f261ede
myeonghwna57 <myeonghwan57@gmail.com>
2023-10-04 19:18:55 +0900

    java reduce 학습

--- JAVA실습/Chapter06/src/ch06/ReduceTest.java
+++ JAVA실습/Chapter06/src/ch06/ReduceTest.java
@@ -18,7 +18,7 @@ public class ReduceTest {
 		System.out.println(Arrays.stream(greetings).reduce("",(s1,s2)->
 			{if(s1.getBytes().length >= s2.getBytes().length ) return s1;
 			else return s2;}
-			));
+			)); //람다식
 		
 		System.out.println(Arrays.stream(greetings).reduce(new CompareString()).get());
 	}
