55259b25c06bc014a40d637d4c4ec9f0a964ab31
myeonghwna57 <myeonghwan57@gmail.com>
2024-03-13 23:25:55 +0900

    git update

--- JAVA실습/Chapter08/src/ch01/MinMaxProblem.java
+++ JAVA실습/Chapter08/src/ch01/MinMaxProblem.java
@@ -1,26 +1,26 @@
 package ch01;
 
 public class MinMaxProblem {
-
+	
 	public static void main(String[] args) {
 
-		int[] numbers = {10, 55, 23, 2, 79, 101, 16, 82, 30, 45};
-		
+		int[] numbers = { 10, 55, 23, 2, 79, 101, 16, 82, 30, 45 };
+
 		int min = numbers[0];
 		int max = numbers[0];
 		int minPos = 0;
 		int maxPos = 0;
-		
-		for(int i=1; i<numbers.length; i++ ) {
-			
-			if(min > numbers[i]) {
+
+		for (int i = 1; i < numbers.length; i++) {
+
+			if (min > numbers[i]) {
 				min = numbers[i];
-				minPos = i+1;
+				minPos = i + 1;
 			}
-			
-			if(max < numbers[i]) {
+
+			if (max < numbers[i]) {
 				max = numbers[i];
-				maxPos = i+1;
+				maxPos = i + 1;
 			}
 		}
 		
---
+++ coding_test/BOJ/Practice03/src/Q1/Test.java
@@ -0,0 +1,17 @@
+package Q1;
+
+import java.util.Scanner;
+
+public class Test {
+    private static final int MAX_POSTS = 100;
+    private static String[][] board = new String[MAX_POSTS][4];
+    private static int postCount = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] str = new String[5];
+        
+        System.out.println(str[0].length());
+    }
+}
